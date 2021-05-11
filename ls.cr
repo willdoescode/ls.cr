e571bbdc045947eea14d87db9eb0ba613588a902
Will Lane <williamlane923@gmail.com>
2021-05-11 16:59:34 -0700

    compactify

--- ls.cr
+++ ls.cr
@@ -1,11 +1,7 @@
 require "colorize"
 
 paths = Array(Path).new
-
-Dir.new(ARGV.first? || ".").each do |path|
-  paths << Path.new(path)
-end
+Dir.new(ARGV.first? || ".").each { |file| paths << Path.new(file) }
 
 paths.sort! { |a, b| (File.directory?(a) == File.directory?(b)) ? 1 : 0 }
-
 paths.each { |file| puts File.directory?(file) ? (file.to_s + "/").colorize(:blue) : file.to_s }
