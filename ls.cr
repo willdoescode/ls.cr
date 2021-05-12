f44603ddd1a0c710457c058cee64101f4d44a8e2
Will Lane <williamlane923@gmail.com>
2021-05-11 17:44:20 -0700

    fetch

--- ls.cr
+++ ls.cr
@@ -1,6 +1,6 @@
 require "colorize"
 
-paths = Dir.new(ARGV.first? || ".").map { |f| Path.new(f) }
+paths = Dir.new(ARGV.fetch(0, ".")).map { |f| Path.new(f) }
 
 paths.sort! { |a, b| (File.directory?(a) == File.directory?(b)) ? 1 : 0 }
 paths.each { |file| puts File.directory?(file) ? (file.to_s + "/").colorize(:blue) : file.to_s }
