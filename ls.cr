3fb1cda5fac2584ee288e0f9b585500c11cd8740
Will Lane <williamlane923@gmail.com>
2021-05-11 17:04:25 -0700

    made better

--- ls.cr
+++ ls.cr
@@ -1,7 +1,6 @@
 require "colorize"
 
-paths = Array(Path).new
-Dir.new(ARGV.first? || ".").each { |file| paths << Path.new(file) }
+paths = Dir.new(ARGV.first? || ".").map { |f| Path.new(f) }
 
 paths.sort! { |a, b| (File.directory?(a) == File.directory?(b)) ? 1 : 0 }
 paths.each { |file| puts File.directory?(file) ? (file.to_s + "/").colorize(:blue) : file.to_s }
